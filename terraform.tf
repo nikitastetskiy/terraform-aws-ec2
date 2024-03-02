d626e67652c320f7f1ed9b9e293164e26aaba7c8
Cesar Fernandez <cfernand@redhat.com>
2024-03-02 10:32:34 +0100

    Added terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "cesarfn4-demo"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
