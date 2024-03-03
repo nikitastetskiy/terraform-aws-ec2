137b1a2fb96621f77b5f18fb87bf7abb5797c0ef
Cesar Fernandez <cfernand@redhat.com>
2024-03-03 12:08:21 +0100

    Renamed variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "region" {
+  type = string
   description = "AWS region"
   default     = "eu-west-1"
 }
 
 variable "instance_type" {
+  type = string
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
 
 variable "instance_name" {
+  type = string
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
