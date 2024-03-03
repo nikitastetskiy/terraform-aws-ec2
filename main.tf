da6faf24c952782c7eca1852c60bb8156b590a71
Cesar Fernandez <cfernand@redhat.com>
2024-03-03 11:10:23 +0100

    Renamed vars

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu_ami" {
+data "aws_ami" "ami" {
   most_recent = true
 
   filter {
@@ -18,40 +18,39 @@ data "aws_ami" "ubuntu_ami" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_vpc" "ubuntu_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "tf_ubuntu_vpc"
+    Name = "${var.instance_name}_vpc"
   }
 }
 
-resource "aws_subnet" "ubuntu_subnet" {
-  vpc_id            = aws_vpc.ubuntu_vpc.id
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "eu-west-1a"
+  availability_zone = "${var.region}a"
 
   tags = {
-    Name = "tf_ubuntu_subnet"
+    Name = "${var.instance_name}_subnet"
   }
 }
 
-resource "aws_network_interface" "ubuntu_network_interface" {
-  subnet_id   = aws_subnet.ubuntu_subnet.id
+resource "aws_network_interface" "network_interface" {
+  subnet_id   = aws_subnet.subnet.id
   private_ips = ["172.16.10.100"]
 
   tags = {
-    Name = "tf_ubuntu_network_interface"
+    Name = "${var.instance_name}_network_interface"
   }
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu_ami.id
+resource "aws_instance" "vm" {
+  ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-  associate_public_ip_address = true
   
   network_interface {
-    network_interface_id = aws_network_interface.ubuntu_network_interface.id
+    network_interface_id = aws_network_interface.network_interface.id
     device_index         = 0
   }
 
