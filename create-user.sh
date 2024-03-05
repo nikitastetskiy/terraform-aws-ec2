10c7d0a317594cad2f5377da4892024f855c0829
Cesar Fernandez <cfernand@redhat.com>
2024-03-05 16:42:50 +0100

    Fixed create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -3,6 +3,6 @@ sudo useradd hashicorp --create-home --shell /bin/bash
 sudo usermod -aG sudo hashicorp
 sudo mkdir /home/hashicorp/.ssh
 sudo chmod 700 /home/hashicorp/.ssh
-sudo cp /ubuntu/.ssh/authorized_keys /home/hashicorp/.ssh/authorized_keys
+sudo cp /home/ubuntu/.ssh/authorized_keys /home/hashicorp/.ssh/authorized_keys
 sudo chmod 600 /home/hashicorp/.ssh/authorized_keys
 sudo chown -R hashicorp:hashicorp /home/hashicorp/.ssh
