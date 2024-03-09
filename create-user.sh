efe9b96dc261dd781d2cd20db822e89cc9953f7c
Cesar Fernandez <cfernand@redhat.com>
2024-03-09 10:23:35 +0100

    Removed user_data

--- create-user.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo useradd hashicorp --create-home --shell /bin/bash
-sudo usermod -aG sudo hashicorp
-sudo mkdir /home/hashicorp/.ssh
-sudo chmod 700 /home/hashicorp/.ssh
-sudo cp /home/ubuntu/.ssh/authorized_keys /home/hashicorp/.ssh/authorized_keys
-sudo chmod 600 /home/hashicorp/.ssh/authorized_keys
-sudo chown -R hashicorp:hashicorp /home/hashicorp/.ssh
