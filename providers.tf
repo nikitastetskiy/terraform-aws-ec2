8641ced797704826195bcce6619a502e8ac49f54
Cesar Fernandez <33290633+cesarfn4@users.noreply.github.com>
2024-03-11 14:02:12 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = "~> 1.2"
 
-  cloud {
-    organization = "cesarfn4-demo"
-
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
