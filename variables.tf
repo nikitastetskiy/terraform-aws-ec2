8ea713d805d0dea60a77db9d572760393a55c480
Luis Roset Giménez <167411942+luis-roset@users.noreply.github.com>
2024-05-21 10:47:28 +0200

    removed defaults

--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
 variable "region" {
   type = string
   description = "AWS region"
-  default     = "eu-west-1"
 }
 
 variable "instance_type" {
   type = string
   description = "EC2 instance type"
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   type = string
   description = "EC2 instance name"
-  default     = "terraform"
 }
 
