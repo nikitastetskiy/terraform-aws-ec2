758e6fa845c64eab2da8378bb388d4a1c25819c7
Luis Roset Giménez <167411942+luis-roset@users.noreply.github.com>
2024-05-21 10:18:00 +0200

    ssh key removed

--- main.tf
+++ main.tf
@@ -81,7 +81,6 @@ resource "aws_vpc_security_group_ingress_rule" "security_group_ingress_rule" {
 resource "aws_instance" "vm" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  key_name               = "${var.instance_name}-key"
   subnet_id              = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.security_group.id]
 
