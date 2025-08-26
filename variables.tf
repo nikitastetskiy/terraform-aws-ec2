cbfb2e11692a8e8e6d22f4945284c5014eb9a4f0
Nikita Stetskiy <42643697+nikitastetskiy@users.noreply.github.com>
2025-08-26 14:56:13 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,23 @@
 variable "region" {
-  type = string
+  type        = string
   description = "AWS region"
+  default     = "eu-west-1"
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "EC2 instance type"
+  default     = "t2.micro"
 }
 
 variable "instance_name" {
-  type = string
+  type        = string
   description = "EC2 instance name"
+  default     = "default-instance"
 }
 
+variable "ami_id" {
+  type        = string
+  description = "AMI ID (must be provided, avoids DescribeImages)"
+  default     = "ami-09b024e886d7bbe74"
+}
